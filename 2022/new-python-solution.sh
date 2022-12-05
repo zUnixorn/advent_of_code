052224994fd8b0db166251af980aef5ef1b0f1cf
zUnixorn <dev@joanscrull.de>
2022-12-05 16:35:58 +0100

    Added script option for automatic download of input

--- 2022/new-python-solution.sh
+++ 2022/new-python-solution.sh
@@ -1,12 +1,40 @@
+#!/bin/bash
+
 set -e
 
 if [ -z "$1" ]; then
-    echo "You need to specify a Day"
+    echo "You need to specify a day"
+    exit 1
+fi
+
+MESSAGE="The day can only be a number between 1 and 24"
+
+case $1 in
+    ''|*[!0-9]*) echo $MESSAGE; exit 1 ;;
+    *) ;;
+esac
+
+if [ "$1" -lt 1 ] || [ "$1" -gt 24 ]; then
+    echo "$MESSAGE"
     exit 1
 fi
 
 # from https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+if [ -z "$TOKEN" ] && [ -f "${SCRIPT_DIR}/../.env" ]; then
+    export "$(xargs < ${SCRIPT_DIR}/../.env)"
+fi
+
+NODOWNLOAD="0"
+
+if [ -z "$TOKEN" ]; then
+    # TODO make continueation optional
+    read -p "token unset, only create templates? (y/N): " -r "NODOWNLOAD"
+    if [[ ! "$NODOWNLOAD" =~ "^[Yy]$" ]]; then
+        NODOWNLOAD="1"
+    fi
+fi
 
 cd "${SCRIPT_DIR}/Python"
 
@@ -15,8 +43,22 @@ if [ -d "$1" ]; then
     exit 1
 fi
 
+if [ "$NODOWNLOAD" = "0" ]; then
+    URL="https://adventofcode.com/2022/day/${1}/input"
+    STATUS=$(curl --cookie "session=$TOKEN" -s -o /dev/null -w "%{http_code}" "$URL")
+
+    if [ "$STATUS" = "404" ]; then
+        echo "The day is not unlocked yet"
+        exit 1
+    fi
+fi
+
 mkdir $1
 cd $1
+
+if [ "$NODOWNLOAD" = "0" ]; then
+    curl --cookie "session=$TOKEN" -s -J -o "input" -w "%{http_code}" "$URL"
+fi
 
 cat << EOF > main.py
 import part1
