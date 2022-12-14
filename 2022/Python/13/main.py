9382c6ce2e023e7389176d38b456d74d2a85bf9b
zUnixorn <dev@joanscrull.de>
2022-12-14 14:25:41 +0100

    switched the eval to a safer alternative

--- 2022/Python/13/main.py
+++ 2022/Python/13/main.py
@@ -1,11 +1,12 @@
 import part1
 import part2
+from ast import literal_eval
 
 if __name__ == "__main__":
     with open("input") as file:
         lines = file.read()
 
     lines = lines.split('\n\n')
-    lines = [[eval(p) for p in l.split('\n')] for l in lines]
+    lines = [[literal_eval(p) for p in l.split('\n')] for l in lines]
     print(part1.solution(lines))
     print(part2.solution(lines))
